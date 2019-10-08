83fba7a5033b9def5491256b4edd0751d2627038
shysimon <49863977+shysimon@users.noreply.github.com>
2019-10-09 05:10:48 +0800

    Update TxtLineReader.java

--- src/manager/TxtLineReader.java
+++ src/manager/TxtLineReader.java
@@ -3,19 +3,14 @@ package manager;
 import model.Line;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.Scanner;
 
 public class TxtLineReader {
     public static ArrayList<Line> read(String path) throws FileNotFoundException {
         ArrayList<Line> r = new ArrayList<>();
-        Scanner input = null;
-        try {
-            input = new Scanner(new InputStreamReader(new FileInputStream(path), "utf-8"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        Scanner input = new Scanner(new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
         while (input.hasNextLine()) {
             String it = input.nextLine();
             if (it.length() == 0) continue;
