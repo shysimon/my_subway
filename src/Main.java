a78b44b0e0aa2c398a0235f071bf530bb6a4a93a
shysimon <49863977+shysimon@users.noreply.github.com>
2019-10-11 06:52:50 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,20 @@
+import javafx.util.Pair;
+import manager.ArgsReader;
 import manager.FindWay;
-import manager.TxtLineReader;
 import model.Line;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Line> lines = new ArrayList<>();
-        OutputStreamWriter output = new OutputStreamWriter(System.out);
-        FindWay map = new FindWay();
-        int type = 0, hasMap = 0;
-        String[] names = new String[2];
-        // write your code here
-        for (int i = 0; i < args.length; i++) {
-            String tmp = args[i];
-            if (tmp.charAt(0) == '-') {
-                try {
-                    if (tmp.compareTo("-map") == 0) {
-                        String filepath;
-                        i++;
-                        filepath = args[i];
-                        if (filepath.charAt(0) == '-')
-                            throw new Exception("缺少-map后的一个参数");
-                        lines = TxtLineReader.read(filepath);
-                        map = new FindWay(lines);
-                        hasMap = 1;
-                    } else if (tmp.compareTo("-o") == 0) {
-                        String filepath;
-                        i++;
-                        filepath = args[i];
-                        if (filepath.charAt(0) == '-')
-                            throw new Exception("缺少-o后的一个参数");
-                        output = new OutputStreamWriter(new FileOutputStream(filepath), StandardCharsets.UTF_8);
-                    } else if (tmp.compareTo("-a") == 0) {
-                        if (type != 0) {
-                            System.out.println("-a,-b都只能出现一次，使用-help参数查看帮助");
-                            return;
-                        }
-                        type = 1;
-                        i++;
-                        String lineName = args[i];
-                        if (lineName.charAt(0) == '-')
-                            throw new Exception("缺少-a后的一个参数");
-                        names[0] = lineName;
-                    } else if (tmp.compareTo("-b") == 0) {
-                        if (type != 0) {
-                            System.out.println("-a,-b都只能出现一次，使用-help参数查看帮助");
-                            return;
-                        }
-                        type = 2;
-                        for (int j = 0; j < 2; j++) {
-                            i++;
-                            String stationName = args[i];
-                            if (stationName.charAt(0) == '-')
-                                throw new Exception("缺少-b后的参数");
-                            names[j] = stationName;
-                        }
-                    } else if (tmp.compareTo("-help") == 0) {
-                        System.out.println(tmp + "参数请单独使用");
-                        return;
-                    } else {
-                        System.out.println("无效参数" + tmp + "，使用-help参数查看帮助");
-                        return;
-                    }
-                } catch (FileNotFoundException e) {
-                    System.out.println(tmp + "后参数错误，文件不存在，使用-help参数查看帮助");
-                    return;
-                } catch (Exception e) {
-                    System.out.println(tmp + "后参数错误，使用-help参数查看帮助");
-                    return;
-                }
-            }
-        }
-        if (hasMap == 0) {
-            System.out.println("请使用-map参数传入地铁数据，使用-help参数查看帮助");
-            return;
-        }
+        Pair<Integer, ArrayList<Object>> in = ArgsReader.argsReader(args);
+        int type = in.getKey();
+        if (type == -1) return;
+        ArrayList<Line> lines = (ArrayList<Line>) in.getValue().get(0);
+        FindWay map = (FindWay) in.getValue().get(1);
+        String[] names = (String[]) in.getValue().get(2);
+        OutputStreamWriter output = (OutputStreamWriter) in.getValue().get(3);
         switch (type) {
             case 0:
                 for (Line it : lines) {
