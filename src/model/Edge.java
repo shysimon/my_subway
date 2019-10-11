3e072119d25d0eeb90be52f3a3227d9bed4c5846
shysimon <49863977+shysimon@users.noreply.github.com>
2019-10-11 09:43:41 +0800

    Add files via upload

---
+++ src/model/Edge.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Edge {
+    private int to, atLine;
+
+    public Edge() {
+    }
+
+    public Edge(int to, int atLine) {
+        this.to = to;
+        this.atLine = atLine;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public int getAtLine() {
+        return atLine;
+    }
+}
---
+++ src/model/NowAt.java
@@ -0,0 +1,16 @@
+package model;
+
+public class NowAt {
+    public int from, atStation, atLine, length, changeLine;
+
+    public NowAt() {
+    }
+
+    public NowAt(int from, int atStation, int atLine, int length, int changeLine) {
+        this.from = from;
+        this.atStation = atStation;
+        this.atLine = atLine;
+        this.length = length;
+        this.changeLine = changeLine;
+    }
+}
